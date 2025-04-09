93c5c59c6e8d2b505f5babfecafcd08c9fa32dd9
Keerthana6739 <keerthisukumar2001@gmail.com>
2025-04-09 10:26:50 +0530

    ll

--- truly_kenya/settings.py
+++ truly_kenya/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-b)le4akgcu!e1cbp*4b+j^h_qr9+t95a8@bd-p7s&n2mz&c0v!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
