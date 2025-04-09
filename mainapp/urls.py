eb2da277336be29a5a1b4a0b6eebbf21421ea6ab
Keerthana6739 <keerthisukumar2001@gmail.com>
2025-04-09 09:59:17 +0530

    first commit

---
+++ mainapp/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    path('index', views.index, name='index'),
+    path('digital', views.digital, name='digital'),
+    path('music', views.music, name='music'),
+    path('satellite', views.satellite, name='satellite'),
+    path('movies', views.movies, name='movies'),
+    path('gaming', views.gaming, name='gaming'),
+    path('elearning', views.elearning, name='elearning'),
+    path('lottery', views.lottery, name='lottery'),
+    path('ecommerce', views.ecommerce, name='ecommerce'),
+    path('tours', views.tours, name='tours'),
+
+
+
+]
---
+++ mainapp/views.py
@@ -0,0 +1,24 @@
+from django.urls import path,include
+from .views import *
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+def digital(request):
+    return render(request, 'digital.html')
+def music(request):
+    return render(request, 'music.html')
+def satellite(request):
+    return render(request, 'satellite.html')
+def movies(request):
+    return render(request, 'movies.html')
+def gaming(request):
+    return render(request, 'gaming.html')
+def elearning(request):
+    return render(request, 'elearning.html')
+def lottery(request):
+    return render(request, 'lottery.html')
+def ecommerce(request):
+    return render(request, 'ecommerce.html')
+def tours(request):
+    return render(request, 'tours.html')
